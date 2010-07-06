1c30ff969477cd844db98b629abde634889a0d4d
Aaron Brashears <aaronbrashears@gmail.com>
2010-07-05 23:17:49 -0700

    refactored to have a class instance do the heavy lifting so that users of the helper can either create a new S3Upload instance through the s3_upload() function and embed the hidden fields in a form built using the more typical rails helpers. maintains API compatible calls to s3_http_upload_tag(). documentation pending.

--- lib/d2s3/view_helpers.rb
+++ lib/d2s3/view_helpers.rb
@@ -2,59 +2,82 @@ require 'base64'
 
 module D2S3
   module ViewHelpers
-    include D2S3::Signature
+    class S3Upload
+      include D2S3::Signature
+
+      attr_accessor :action, :bucket, :upload_uri
+
+      def initialize(options)
+        @bucket          = D2S3::S3Config.bucket
+        @access_key_id   = D2S3::S3Config.access_key_id
+        @key             = options[:key] || ''
+        @filename        = options[:filename] || '${filename}'
+        @content_type    = options[:content_type] || '' # Defaults to binary/octet-stream if blank
+        @redirect        = options[:redirect] || '/'
+        @acl             = options[:acl] || 'public-read'
+        @expiration_date = (options[:expiration_date] || 10.hours).from_now.utc.strftime('%Y-%m-%dT%H:%M:%S.000Z')
+        @max_filesize    = options[:max_filesize] || 1.megabyte
+        @min_filesize    = options[:min_filesize] || 1.byte
+        @protocol        = options[:secure] ? 'https' : 'http'
+        @cname           = options[:cname] || false
+
+        # CNAME cannot use https and bucket mut be the same as the
+        # Host headeraccording to S3 documentation.
+        if @protocol == true and @cname == true
+          raise ArgumentError, "Cannot be both secure and use cname" 
+        end
+        @host = @cname ? @host = "#{@bucket}" : "#{@bucket}.s3.amazonaws.com"
+        @key_file = "#{@key}/#{@filename}"
+        @upload_uri = "#{@protocol}://#{@host}/#{@file_key}"
+        @action = "#{@protocol}://#{@host}/"
+      end
+
+      def elements
+        policy = Base64.encode64(
+          "{'expiration': '#{@expiration_date}',
+            'conditions': [
+              {'bucket': '#{@bucket}'},
+              ['starts-with', '$key', '#{@key}'],
+              {'acl': '#{@acl}'},
+              {'success_action_redirect': '#{@redirect}'},
+              ['starts-with', '$Content-Type', '#{@content_type}'],
+              ['content-length-range', #{@min_filesize}, #{@max_filesize}]
+            ]
+          }").gsub(/\n|\r/, '')
+          signature = b64_hmac_sha1(D2S3::S3Config.secret_access_key, policy)
+          out = ""
+          out << %(
+    <input type="hidden" name="key" value="#{@key_file}" \>
+    <input type="hidden" name="AWSAccessKeyId" value="#{@access_key_id}" \>
+    <input type="hidden" name="acl" value="#{@acl}" \>
+    <input type="hidden" name="success_action_redirect" value="#{@redirect}" \>
+    <input type="hidden" name="policy" value="#{policy}" \>
+    <input type="hidden" name="signature" value="#{signature}" \>
+    <input type="hidden" name="Content-Type" value="#{@content_type}" \>
+        )
+      end
+
+    end
+
+    def s3_upload(options = {})
+      S3Upload.new(options)
+    end
 
     def s3_http_upload_tag(options = {})
-      bucket          = D2S3::S3Config.bucket
-      access_key_id   = D2S3::S3Config.access_key_id
-      key             = options[:key] || ''
-      filename        = options[:filename] || '${filename}'
-      content_type    = options[:content_type] || '' # Defaults to binary/octet-stream if blank
-      redirect        = options[:redirect] || '/'
-      acl             = options[:acl] || 'public-read'
-      expiration_date = (options[:expiration_date] || 10.hours).from_now.utc.strftime('%Y-%m-%dT%H:%M:%S.000Z')
-      max_filesize    = options[:max_filesize] || 1.megabyte
-      min_filesize    = options[:min_filesize] || 1.byte
-      submit_button   = options[:submit_button] || '<input type="submit" value="Upload">'
-      protocol        = options[:secure] ? 'https' : 'http'
-      cname           = options[:cname] || false
-
-      # CNAME cannot use https and bucket mut be the same as the
-      # Host headeraccording to S3 documentation.
-      host = cname ? host = "#{bucket}" : "#{bucket}.s3.amazonaws.com"
-      file_key = "#{key}/#{filename}"
-      upload_uri = "#{protocol}://#{host}/#{file_key}"
-
+      uploader = s3_upload(options)
+      submit_button  = options[:submit_button] || '<input type="submit" value="Upload">'
       options[:form] ||= {}
       options[:form][:id] ||= 'upload-form'
       options[:form][:class] ||= 'upload-form'
-
-      policy = Base64.encode64(
-        "{'expiration': '#{expiration_date}',
-          'conditions': [
-            {'bucket': '#{bucket}'},
-            ['starts-with', '$key', '#{key}'],
-            {'acl': '#{acl}'},
-            {'success_action_redirect': '#{redirect}'},
-            ['starts-with', '$Content-Type', '#{content_type}'],
-            ['content-length-range', #{min_filesize}, #{max_filesize}]
-          ]
-        }").gsub(/\n|\r/, '')
-
-        signature = b64_hmac_sha1(D2S3::S3Config.secret_access_key, policy)
-        out = ""
-        out << %(
-          <form action="#{protocol}://#{host}/" method="post" enctype="multipart/form-data" id="#{options[:form][:id]}" class="#{options[:form][:class]}" style="#{options[:form][:style]}" \>
-          <input type="hidden" name="key" value="#{file_key}" \>
-          <input type="hidden" name="AWSAccessKeyId" value="#{access_key_id}" \>
-          <input type="hidden" name="acl" value="#{acl}" \>
-          <input type="hidden" name="success_action_redirect" value="#{redirect}" \>
-          <input type="hidden" name="policy" value="#{policy}" \>
-          <input type="hidden" name="signature" value="#{signature}" \>
-          <input type="hidden" name="Content-Type" value="#{content_type}" \>
-          <input name="file" type="file" \>#{submit_button}
-          </form>
-        )
+      options[:form][:target] ||= ''
+      out = ""
+      out << %(
+  <form action="#{uploader.action}" method="post" enctype="multipart/form-data" id="#{options[:form][:id]}" class="#{options[:form][:class]}" style="#{options[:form][:style]}" target="#{options[:form][:target]}"\>
+    #{uploader.elements}
+    <input name="file" type="file" \>
+    #{submit_button}
+  </form>
+      )
     end
   end
 end
